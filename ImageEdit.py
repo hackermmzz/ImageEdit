31f85edebb3b2c3186df1efda2f4c58e2518194c
ähackermmzz <204998347@qq.com>
2025-09-11 08:53:03 +0000

    lhw

--- ImageEdit.py
+++ ImageEdit.py
@@ -1,10 +1,11 @@
-import os
-from PIL import Image
 import torch
-from tools.prompt_utils import polish_edit_prompt
 from diffusers import QwenImageEditPipeline
 import random
 import numpy as np
+from Tips import *
+from Model import AnswerImage
+import json
+from VLM import AnswerImage
 # 配置4bit量化参数
 QUANT_CONFIG = {
             "load_in_4bit": True,
@@ -17,26 +18,68 @@ pipeline = QwenImageEditPipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
     **QUANT_CONFIG
 )
-print("pipeline loaded with 4bit quantization")
 
 # 不需要再调用to("cuda")，因为device_map="auto"已经处理了设备分配
 pipeline.set_progress_bar_config(disable=None)
-while True:
-    path=input("path:")
-    prompt=input("prompt:")
-    image = Image.open(path).convert("RGB")
-    prompt = polish_edit_prompt(prompt, image)
+###############################优化指令
+def polish_edit_prompt(img,prompt):
+    prompt = EDIT_SYSTEM_PROMPT.format(prompt)
+    success=False
+    while not success:
+        try:
+            result = AnswerImage([img],prompt)
+            if isinstance(result, str):
+                result = result.replace('```json','')
+                result = result.replace('```','')
+                result = json.loads(result)
+            else:
+                result = json.loads(result)
+            polished_prompt = result['Rewritten']
+            polished_prompt = polished_prompt.strip()
+            polished_prompt = polished_prompt.replace("\n", " ")
+            success = True
+        except Exception as e:
+            print(f"[Warning] Error during API call: {e}")
+    return polished_prompt
+###################################图像编辑
+def ImageEditApi(image,prompt:str,neg_prompt=" "):
     inputs = {
         "image": image,
         "prompt": prompt,
         "generator": torch.manual_seed(random.randint(0,np.iinfo(np.int32).max)),
         "true_cfg_scale": 4.0,
-        "negative_prompt": " ",
+        "negative_prompt": neg_prompt,
         "num_inference_steps": 50,
     }
-
     with torch.inference_mode():
         output = pipeline(** inputs)
         output_image = output.images[0]
-        output_image.save("output_image_edit.png")
-        print("image saved at", os.path.abspath("output_image_edit.png"))
+    return output_image.convert("RGB")
+    '''
+    imagesResponse = client.images.generate(
+        model="doubao-seededit-3-0-i2i-250628",
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        image=encode_image(image),
+        seed=random.randint(0,np.iinfo(np.int32).max),
+        guidance_scale=8.0,
+        size="adaptive",
+        watermark=True
+    )
+    #下载图片
+    image_url=imagesResponse.data[0].url
+    headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+    response = requests.get(image_url, headers=headers, timeout=30)
+    response.raise_for_status() #检查请求是否成功
+    #将二进制数据转换为PIL Image对象
+    image = Image.open(BytesIO(response.content))
+    return image.convert("RGB")
+    '''
+###############################给定指令进行编辑
+def EditImage(image,prompt:str,negative_prompt="",polish_prompt=True):
+    if polish_prompt:
+        prompt=polish_edit_prompt(image,prompt)
+    return ImageEditApi(image,prompt,negative_prompt)
+    
---
+++ LLM.py
@@ -0,0 +1,80 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from Tips import *
+import json
+#############################################
+LLMProcessor=None
+LLMModel=None
+#############################################加载LLM模型
+def LoadLLM():
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,  # 启用 4bit 量化
+            bnb_4bit_use_double_quant=True,  # 双量化，进一步减少显存占用
+            bnb_4bit_quant_type="nf4",  # 推荐的量化类型（比 fp4 更适合自然语言）
+            bnb_4bit_compute_dtype=torch.bfloat16  # 计算时用 bfloat16，保精度
+        )
+        global LLMProcessor,LLMModel
+        LLMProcessor = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-0528-Qwen3-8B")
+        LLMModel = AutoModelForCausalLM.from_pretrained(
+            "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B",
+            quantization_config=bnb_config, 
+            device_map="auto", 
+            trust_remote_code=True
+        ).to(DEVICE).eval()
+#########################################调用
+def AnswerText(question:str):
+    ##########################调用豆包大模型
+    try:
+        # Non-streaming:
+        completion = client.chat.completions.create(
+            # 指定您创建的方舟推理接入点 ID，此处已帮您修改为您的推理接入点 ID
+            model="doubao-1-5-pro-256k-250115",
+            messages=[
+                {"role": "user", "content": f"{question}"},
+            ],
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        Debug(e)
+        return AnswerText(question)
+    ############################
+        processor=LLM.processor
+        model=LLM.model
+        # 4. 定义对话内容
+        messages = [
+            {"role": "user", "content":"{}".format(question)},
+        ]
+
+        # 5. 处理输入（转为模型可识别的格式，并移到模型所在设备）
+        inputs = processor.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_dict=True,
+            enable_thinking=False,
+            return_tensors="pt",
+        ).to(DEVICE)  # 确保输入与模型在同一设备（GPU）
+
+        # 6. 生成回复（添加推理参数优化效果）
+        outputs = model.generate(
+            **inputs,
+            max_new_tokens=65535,  # 最大生成 token 数
+            temperature=0.7,  # 随机性（0-1，越低越确定）
+            do_sample=True,  # 启用采样生成（更自然）
+            pad_token_id=processor.eos_token_id  # 填充 token 指定
+        )
+
+        # 7. 解码并打印回复（只取新增生成的部分）
+        response = processor.decode(
+            outputs[0][inputs["input_ids"].shape[-1]:],  # 跳过输入部分
+            skip_special_tokens=True  # 忽略特殊 token（如 <bos> <eos>）
+        )
+        return response
+#指令优化
+def OptmEditInstruction(negPrompt:str,instruction:str):
+    res=AnswerText(InsOptim_Prompt.format(negPrompt,instruction))
+    try:
+        data=json.loads(res)
+        return data["new_instruction"]
+    except Exception as e:
+        return ""
